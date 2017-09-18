0c4da8b02fe4bed67db50ece3214cd26e9dcc67f
Joshua Woodcock <joshuawoodcock91@gmail.com>
2017-09-18 12:16:41 +0100

    Works. Although it takes some time to run.

--- bruteForce.py
+++ bruteForce.py
@@ -3,32 +3,35 @@
 
 import PyPDF2
 
-pdfReader = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
-
 # Get dictionary file
 dictFile = open('C:\\Users\\Josh\\Anaconda3\\automate-the-boring-stuff\\dictionary.txt')
 
 # Open dictionary file
 with open('C:\\Users\\Josh\\Anaconda3\\automate-the-boring-stuff\\dictionary.txt') as dictFile:
-     dict = dictFile.read()
+    dictionary = dictFile.read()
 
 # Create list of passwords
 passwordList = []
 
 # For each word in dictionary add lower and UPPER case versions to password list
-for word in dictFile:
+for word in dictionary.split():
 
     lowerCase = word.lower()
     passwordList.append(lowerCase)
 
     upperCase = word.upper()
-    passwordList.append(lowerCase)
+    passwordList.append(upperCase)
+
+print(passwordList)
+
+pdfReader = PyPDF2.PdfFileReader(open('encrypted.pdf', 'rb'))
 
 # Run all passwords in list through decryption
 for password in passwordList:
     if pdfReader.decrypt(password) == 1:
-        print('The password is ' + password)
+        solved = password
         break
     else:
         continue
+print('The password is ' + solved)
 
